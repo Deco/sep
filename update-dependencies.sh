b1db1737bed9046e710ee2243f3483eade671f0c
Declan White <declanw@concap.com.au>
2014-08-25 20:18:56 +0800

    Updated dependency update script

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -1,2 +1,11 @@
-git subtree pull --prefix dependencies/msgpack-c/ https://github.com/msgpack/msgpack-c.git master --squash
-git subtree pull --prefix dependencies/libevent/  https://github.com/libevent/libevent.git master --squash
+git subtree pull --squash \
+    --prefix dependencies/msgpack-c/ \
+    https://github.com/msgpack/msgpack-c.git master \
+    --message="Updating dependencies/msgpack-c" \
+;
+
+git subtree pull --squash \
+    --prefix dependencies/libevent/ \
+    https://github.com/libevent/libevent.git master \
+    --message="Updating dependencies/libevent" \
+;
