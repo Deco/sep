019acf4cee7c8431bb6098b23e61b55c436c02a9
Declan White <declanw@concap.com.au>
2014-08-25 15:51:32 +0800

    Added script to update subtree dependencies

---
+++ update-dependencies.sh
@@ -0,0 +1,2 @@
+git subtree pull --prefix dependencies/msgpack-c/ https://github.com/msgpack/msgpack-c.git master --squash
+git subtree pull --prefix dependencies/libevent/  https://github.com/libevent/libevent.git master --squash
